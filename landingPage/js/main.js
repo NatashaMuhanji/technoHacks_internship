78712c4625845f7bf0cce2c2507ef797ad0882fe
Natasha Muhanji <125664721+NatashaMuhanji@users.noreply.github.com>
2023-08-11 13:14:14 +0300

    changes on js

--- landingPage/js/main.js
+++ landingPage/js/main.js
@@ -1,17 +1,17 @@
 
-//-----------navigation bar
+/*-----------navigation bar-----------------*/
 const bar = document.querySelector("#bar");
 const nav = document.querySelector("#navbar");
 const close = document.querySelector("#close");
 
-//show navigation bar
+/*--------------shows the navigation bar------------*/
 if (bar) {
     bar.addEventListener("click", function () {
         nav.classList.add("active");
     });
 }
 
-//close navigation bar
+/*-----------closes the navigation bar---------------*/
 if (close) {
     close.addEventListener("click", function () {
         nav.classList.remove("active");
